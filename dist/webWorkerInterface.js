4ee1cde50dbe6e73066b7939d37132b6ec4dc795
melgrove <oliver@melgrove.com>
2023-11-06 15:34:37 -0800

    rm dist

--- dist/webWorkerInterface.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.getMove = void 0;
-const engine_1 = require("./engine");
-Object.defineProperty(exports, "getMove", { enumerable: true, get: function () { return engine_1.getMove; } });
